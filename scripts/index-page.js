2e25c093c7f2790951db018b91a269ca319a64f3
dat <andyzdinh08@gmail.com>
2023-07-22 10:32:12 -0700

    add axios changes- still have error

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,4 +1,4 @@
-const images = [
+let images = [
 	{
 		url: "./assets/images/Photo-gallery-1.jpg",
 	},
@@ -66,5 +66,33 @@ const createImageContainer = (imgUrl) => {
 	parentEl.appendChild(photoGalleryItem);
 };
 
+const getPetImages = () => {
+	// // Make Api Call using axios
+	// axios
+	// .get("https://api.thedogapi.com/v1/images/search?limit=10")
+	// .then((response) => {
+	// 	// store data in images
+	// 	images = response.data;
+	// })
+	// // then call showGallery
+	// .catch ((e) => console.error(e));
+	axios
+	.all([
+	axios.get("https://api.thedogapi.com/v1/images/search?limit=10"),
+	axios.get("https://api.thecatapi.com/v1/images/search?limit=10 "),
+	])
+	.then((response) => {
+		// store data in images
+		images = response(0).data;
+		images.concat(response[0].data);
+		// then call showGallery();
+		showGallery();
+	})
+	.catch((e) => console.error(e));
+};
+
+
 // Onload show gallery
-showGallery();
+// Get all images from the api
+// then call showGallery();
+getPetImages();
